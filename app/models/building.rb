269da36b30e9f318de7b12e946027bb5bc33e32f
rafal <jarorafo@gmail.com>
2022-01-03 23:05:06 +0100

    Buildings totals

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,8 +1,22 @@
 class Building < ApplicationRecord
-    has_many :flats
-    has_many :tenants, through: :flats
+  has_many :flats
+  has_many :tenants, through: :flats
 
-    
+  def flats_taken_display
+    all = self.flats.count
+    taken = Flat.where(taken: true, building_id: self.id).count
+    ret = "#{taken}/#{all}"
+  end
+
+  def total_due
+    debt_array = Flat.where(building_id: self.id).pluck(:debt).compact
+    debt = debt_array.reduce(:+)
+  end
+
+  def total_revenue
+    revenue_array = Flat.where(building_id: self.id).pluck(:rent).compact
+    revenue = revenue_array.reduce(:+)
+  end
 end
 
 # == Schema Information
